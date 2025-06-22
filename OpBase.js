001870bef459424f8de77da1269e68466dbfc12c
kimchaip <kim_chaip@hotmail.com>
2025-06-22 18:52:02 +0700

    Update setStatus method to check not exist Opnote before append

--- OpBase.js
+++ OpBase.js
@@ -271,7 +271,9 @@ var ob = {
       let v = e.field("Visit").length>0 ? e.field("Visit")[0] : null
       if(v) {
         v.set("Status", "Not")
-        v.set("Rx", v.field("Rx")+(v.field("Rx")?"\n":"")+dt.toDateShort(today)+e.field("OpNote"))  // set visit Rx to OpNote
+        if(v.field("OpNote") && v.field("OpNote").search(/\n*.*ไม่ทำ.*/)==-1) {
+          v.set("Rx", v.field("Rx")+(v.field("Rx")?"\n":"")+dt.toDateShort(today)+e.field("OpNote"))  // set visit Rx to OpNote
+        }
         v.set("DCDate", null)           // clear discharge date
         let p = v.field("Patient").length>0 ? v.field("Patient")[0] : null
         if(p) {                         // if this is the last visit
