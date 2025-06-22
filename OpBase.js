96e83bc585ba0faced7ffc16e83a229c663162bc
kimchaip <kim_chaip@hotmail.com>
2025-06-22 15:32:42 +0700

    Fix case sensitivity in toDateISO method for getMonth and getDate

--- OpBase.js
+++ OpBase.js
@@ -21,7 +21,7 @@ var dt = {
   },
   toDateISO : function(date) {
     if(this.isDate(date)) {
-      return date.getFullYear()+"-"+("0"+(date.getmonth()+1)).slice(-2)+"-"+("0"+date.getdate()).slice(-2)
+      return date.getFullYear()+"-"+("0"+(date.getMonth()+1)).slice(-2)+"-"+("0"+date.getDate()).slice(-2)
     }
     else {
       return ""
