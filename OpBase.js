647d06916e681b43411bdf08f126d1dff7abe50f
kimchaip <kim_chaip@hotmail.com>
2025-06-26 15:39:54 +0700

    log type of lastModTime

--- OpBase.js
+++ OpBase.js
@@ -179,7 +179,7 @@ var vs = {
       log("vss:"+vss.length)
       if(vss.length>0) {
         log(vss[vss.length-1].lastModifiedTime)
-        log(dt.isDate(vss[vss.length-1].lastModifiedTime))
+        log(typeof (vss[vss.length-1].lastModifiedTime))
         let vstoday = vss.filter(v=>dt.toDateISO(v.lastModifiedTime)==dt.toDateISO((today)))
         vstoday.sort((a,b)=>a.lastModifiedTime>b.lastModifiedTime)
         log("vstoday:"+vstoday.length)
